6f6a2058a9c8e4a627604e75854bd6c338137d98
nkaffine <nicholas.kaffine@gmail.com>
2018-05-29 08:31:22 -0400

    adding tests for outer api layer of discoverers

---
+++ test/owaygo_web/controllers/users/create_discoverer_test.exs
@@ -0,0 +1,58 @@
+defmodule Owaygo.Discoverers.CreateTest do
+  use OwaygoWeb.ConnCase
+
+  @valid_username "nkaffine"
+  @valid_fname "Nick"
+  @valid_lname "Kaffine"
+  @valid_email "nicholas.kaffine@gmail.com"
+  @valid_create %{username: @valid_username, fname: @valid_fname,
+  lname: @valid_lname, email: @valid_email}
+  @valid_reason "I want to be a discoverer because I enjoy food and finding new places to eat."
+
+  #Creates a user and returns the user_id
+  defp create_user() do
+    conn = build_conn() |> post("/api/v1/user", @valid_create)
+    body = conn |> response(201) |> Poison.decode!
+    body["id"]
+  end
+
+  #Creates an discoverer application for the user with the given id and returns
+  #the id of the application
+  defp apply_discoverer(id) do
+    attrs = %{id: id, reason: @valid_reason}
+    conn = build_conn() |> post("api/v1/user/discoverer/apply", attrs)
+    body = conn |> response(201) |> Poison.decode!
+    body["id"]
+  end
+
+  defp get_application(id) do
+    conn = build_conn() |> get(discoverer_application_path(build_conn(), :show, id))
+    body = conn |> response(201) |> Poison.decode!
+    body
+  end
+
+  test "provided valid parameters, accepts input and produces valid response" do
+    id = create_user()
+    app_id = apply_discoverer(id)
+    attrs = %{id: id}
+    conn = build_conn() |> post("/api/v1/admin/discoverer", attrs)
+    body = conn |> response(201) |> Poison.decode!
+    assert body["id"] |> is_integer
+    assert body["id"] == id
+    assert body["discoverer_since"] == Date.utc_today |> to_string
+    assert body["balance"] === 0
+    body = get_application(app_id)
+    assert body["id"] == app_id
+    assert body["user_id"] == id
+    assert body["reason"] == @valid_reason
+    assert body["date"] == Date.utc_today |> to_string
+    assert body["status"] == "approved"
+  end
+
+  test "given invalid input, rejects and produces error message" do
+    attrs = %{id: 123}
+    conn = build_conn() |> post("/api/v1/admin/discoverer", attrs)
+    body = conn |> response(400) |> Poison.decode!
+    assert body["id"] == ["user does not exist"]
+  end
+end
