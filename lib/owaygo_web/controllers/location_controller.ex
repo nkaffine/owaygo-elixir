6dfab3d0b27ec7a94d860755f77de6863dbac0f2
nkaffine <nicholas.kaffine@gmail.com>
2018-05-30 11:45:39 -0400

    implementing skeleton of location controller

--- lib/owaygo_web/controllers/location_controller.ex
+++ lib/owaygo_web/controllers/location_controller.ex
@@ -1,4 +1,20 @@
 defmodule OwaygoWeb.LocationController do
   use OwaygoWeb, :controller
 
+  alias Owaygo.Location.Create
+  alias OwaygoWeb.Errors
+
+  def create(conn, params) do
+    attrs = %{lat: params["lat"], lng: params["lng"], name: params["name"],
+    discoverer: params["discoverer"]}
+    case Create.call(%{params: attrs}) do
+      {:ok, location} -> render_location(conn, location)
+      {:error, changeset} -> Errors.render_error(conn, changeset)
+    end
+  end
+
+  defp render_location(conn, location) do
+    
+  end
+
 end
