8f2c281108965bfe8a45d598e3653bca1201ce99
nkaffine <nicholas.kaffine@gmail.com>
2018-05-31 10:24:15 -0400

    fixing foreign key for discoverers

--- lib/owaygo/discoverer.ex
+++ lib/owaygo/discoverer.ex
@@ -1,10 +1,11 @@
 defmodule Owaygo.Discoverer do
   use Ecto.Schema
 
+  @primary_key false
   schema "discoverer" do
     field :discoverer_since, :date, default: Ecto.Date.cast!(Date.utc_today)
     field :balance, :float, default: 0.0
-    belongs_to :user, Owaygo.User
+    belongs_to :user, Owaygo.User, foreign_key: :id, primary_key: true
   end
 
 end
