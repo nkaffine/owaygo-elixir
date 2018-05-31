1b882985360d4c981192ce643d4dc1707ccd80e0
nkaffine <nicholas.kaffine@gmail.com>
2018-05-31 10:09:13 -0400

    fixing foreign key issue with email update

--- lib/owaygo/email_update.ex
+++ lib/owaygo/email_update.ex
@@ -1,12 +1,12 @@
 defmodule Owaygo.EmailUpdate do
   use Ecto.Schema
 
+  @primary_key false
   schema "email_update" do
-    field :email, :string
+    field :email, :string, primary_key: true
     field :verification_date, :date
     field :verification_code, :string
-    belongs_to :user, Owaygo.User
-
+    belongs_to :user, Owaygo.User, foreign_key: :id, primary_key: true
     timestamps()
   end
 end
