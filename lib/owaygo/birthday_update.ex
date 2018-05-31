5de17a38706be02ec48976e54bbc6c6ceed6e6cb
nkaffine <nicholas.kaffine@gmail.com>
2018-05-31 10:16:10 -0400

    fixing foreign keys for birthday update

--- lib/owaygo/birthday_update.ex
+++ lib/owaygo/birthday_update.ex
@@ -1,9 +1,10 @@
 defmodule Owaygo.BirthdayUpdate do
   use Ecto.Schema
 
+  @primary_key false
   schema "birthday_update" do
-    field :birthday, :date
+    field :birthday, :date, primary_key: true
     field :date, :utc_datetime, default: Ecto.DateTime.utc
-    belongs_to :user, Owaygo.User
+    belongs_to :user, Owaygo.User, foreign_key: :id, primary_key: true
   end
 end
