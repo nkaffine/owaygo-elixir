c78a1f7ab74cc05181801840bc5e4f1455eb8ac2
nkaffine <nicholas.kaffine@gmail.com>
2018-05-31 08:53:50 -0400

    fixing issue with setting default value to current date

--- lib/owaygo/discoverer.ex
+++ lib/owaygo/discoverer.ex
@@ -2,7 +2,7 @@ defmodule Owaygo.Discoverer do
   use Ecto.Schema
 
   schema "discoverer" do
-    field :discoverer_since, :date, default: Ecto.Date.utc()
+    field :discoverer_since, :date, default: Ecto.Date.cast!(Date.utc_today)
     field :balance, :float, default: 0.0
     belongs_to :user, Owaygo.User
   end
