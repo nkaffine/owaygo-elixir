e8c5bfa43369e390e31039cddcfc2a1bf0cf0010
nkaffine <nicholas.kaffine@gmail.com>
2018-05-31 10:22:01 -0400

    fixing foreign keys for discoverer application

--- lib/owaygo/discoverer_application.ex
+++ lib/owaygo/discoverer_application.ex
@@ -2,11 +2,11 @@ defmodule Owaygo.DiscovererApplication do
   use Ecto.Schema
 
   schema "discoverer_application" do
-    field :user_id, :id
     field :date, :date, default: Ecto.Date.cast!(Date.utc_today)
     field :reason, :string
     field :status, :string, default: "pending"
     field :message, :string, virtual: true
+    belongs_to :user, Owaygo.User
     timestamps()
   end
 
