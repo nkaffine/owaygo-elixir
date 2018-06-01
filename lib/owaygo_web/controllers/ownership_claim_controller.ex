359e1b1bb6076a1fb994bbb751ff5431229adfdb
nkaffine <nicholas.kaffine@gmail.com>
2018-06-01 17:34:38 -0400

    adding render function to ownership claim controller

--- lib/owaygo_web/controllers/ownership_claim_controller.ex
+++ lib/owaygo_web/controllers/ownership_claim_controller.ex
@@ -6,15 +6,18 @@ defmodule OwaygoWeb.OwnershipClaimController do
 
 
   def create(conn, params) do
-    attrs = %{user_id: params["user_id"], location_id: params["locaton_id"]}
-    case OwnershipClaim.create(%{params: attrs}) do
+    attrs = %{user_id: params["user_id"], location_id: params["location_id"]}
+    case OwnershipClaim.call(%{params: attrs}) do
       {:ok, ownership_claim} -> render_ownership_claim(conn, ownership_claim)
       {:error, changeset} -> Errors.render_error(conn, changeset)
     end
   end
 
   def render_ownership_claim(conn, ownership_claim) do
-    
+    {:ok, body} = %{user_id: ownership_claim.user_id,
+    location_id: ownership_claim.location_id, date: ownership_claim.date,
+    status: ownership_claim.status} |> Poison.encode
+    conn |> resp(201, body)
   end
 
 end
