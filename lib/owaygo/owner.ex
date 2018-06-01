0dc5279baa908846cdbfa3a01a46ed9de86831ca
nkaffine <nicholas.kaffine@gmail.com>
2018-06-01 13:38:00 -0400

    adding owner schema and owner table to the database

---
+++ lib/owaygo/owner.ex
@@ -0,0 +1,15 @@
+defmodule Owaygo.Owner do
+  use Ecto.Schema
+  alias Owaygo.User
+  alias Owaygo.Location
+
+  schema "owner" do
+    field :owner_balance, :float, default: 0.0
+    field :withdrawal_amount, :float
+    field :withdrawal_rate, :integer
+    field :claimer_id, :id, virtual: true
+    belongs_to :location, Location
+    belongs_to :user, User
+  end
+
+end
---
+++ priv/repo/migrations/20180601160141_add_owner_table.exs
@@ -0,0 +1,18 @@
+defmodule Owaygo.Repo.Migrations.AddOwnerTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:owner) do
+      add :owner_balance, :float
+      add :withdrawal_amount, :float
+      add :withdrawal_rate, :integer
+      add :user_id, references(:user)
+      add :location_id, references(:location)
+    end
+    create index(:owner, [:user_id, :location_id])
+  end
+
+  def down do
+    drop table(:owner)
+  end
+end
