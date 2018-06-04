e5c4bcd4401b10949d024d9e8324fc2e7509644c
nkaffine <nicholas.kaffine@gmail.com>
2018-06-04 17:35:37 -0400

    updating controller with show function

--- lib/owaygo_web/controllers/location_controller.ex
+++ lib/owaygo_web/controllers/location_controller.ex
@@ -2,6 +2,7 @@ defmodule OwaygoWeb.LocationController do
   use OwaygoWeb, :controller
 
   alias Owaygo.Location.Create
+  alias Location.Show
   alias OwaygoWeb.Errors
 
   def create(conn, params) do
@@ -13,12 +14,26 @@ defmodule OwaygoWeb.LocationController do
     end
   end
 
+  def show(conn, %{"id" => id}) do
+    case Show.call(%{params: %{id: id}}) do
+      {:ok, location} -> render_show(conn, location)
+      {:error, changeset} -> Errors.render_error(conn, changeset)
+    end
+  end
+
   defp render_location(conn, location) do
     {:ok, body} = %{id: location.id, lat: location.lat, lng: location.lng,
     name: location.name, discovery_date: location.discovery_date,
     discoverer_id: location.discoverer_id, claimer_id: location.claimer_id,
     type: location.type} |> Poison.encode
-    #need to add owner and type when they are implemented
+    conn |> resp(201, body)
+  end
+
+  defp render_show(conn, location) do
+    {:ok, body} = %{id: location.id, lat: location.lat, lng: location.lng,
+    name: location.name, discovery_date: location.discovery_date,
+    discoverer_id: location.discoverer_id, claimer_id: location.claimer_id,
+    type: location.type, owner: location.owner} |> Poison.encode
     conn |> resp(201, body)
   end
 
