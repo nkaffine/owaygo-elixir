e1bbd2e82b256c6474817394b02c68a5e6296532
nkaffine <nicholas.kaffine@gmail.com>
2018-06-04 09:06:19 -0400

    adding tests for outer layer of API for creating location types

---
+++ test/owaygo_web/controllers/locations/create_location_type_test.exs
@@ -0,0 +1,21 @@
+defmodule OwaygoWeb.Location.TestCreateLocationType do
+  use OwaygoWeb.ConnCase
+
+  @typename "restaurant"
+  @create %{name: @typename}
+
+  test "return valid output when given valid input" do
+    conn = build_conn() |> post("/api/v1/admin/location/type", @create)
+    body = conn |> response(201) |> Poison.decode!
+    assert body["id"] |> is_integer
+    assert body["id"] > 0
+    assert body["name"] == @typename
+  end
+
+  test "throws error when given invalid input" do
+    conn = build_conn() |> post("/api/v1/admin/location/type", %{name: "1241512"})
+    body = conn |> response(400) |> Poison.decode!
+    assert body["name"] == ["is invalid"]
+  end
+
+end
