4625f38dff360633c7643eda5891b24b141da899
nkaffine <nicholas.kaffine@gmail.com>
2018-06-05 09:59:08 -0400

    implementing show location function

--- lib/owaygo/contexts/location/show.ex
+++ lib/owaygo/contexts/location/show.ex
@@ -4,6 +4,7 @@ defmodule Owaygo.Location.Show do
   alias Owaygo.Repo
   alias Owaygo.Location
   alias Owaygo.OwnershipClaim
+  alias Owaygo.LocationType
 
   def call(%{params: params}) do
     case Repo.transaction fn ->
@@ -11,8 +12,15 @@ defmodule Owaygo.Location.Show do
       |> load_location_info
       |> load_owner
     end do
-      {:error, value} -> value
-      value -> value
+      {:error, value} -> value  |> translate_from_transaction
+      {:ok, value} -> value |> translate_from_transaction
+    end
+  end
+
+  defp translate_from_transaction(value) do
+    case value do
+      {:error, value} -> {:error, value}
+      value -> {:ok, value}
     end
   end
 
@@ -44,18 +52,29 @@ defmodule Owaygo.Location.Show do
     end
   end
 
+  # returns the name of the location type with the given id.
+  defp translate_type(id) do
+    if(id != nil) do
+      Repo.one!(from t in LocationType, where: t.id == ^id, select: t.name)
+    else
+      nil
+    end
+  end
+
   defp load_owner(location) do
     case location do
       {:error, value} -> {:error, value}
-      _ -> location |> Map.from_struct |> Map.put(:owner, get_owner(location.id))
+      _ -> location |> Map.from_struct
+      |> Map.put(:owner_id, get_owner(location.id))
+      |> Map.put(:type, location.type |> translate_type)
     end
   end
 
   #either returns the user_id of the owner if there is one or returns nil
   defp get_owner(location_id) do
-    num = Repo.one!(from o in OwnershipClaim, where: o.location_id == ^location_id, select: count(o.id))
+    num = Repo.one!(from o in OwnershipClaim, where: o.location_id == ^location_id, select: count(o.user_id))
     cond do
-      num == 0 -> {:error, %{id: ["location does not exist"]}}
+      num == 0 -> nil
       num == 1 -> Repo.one!(from o in OwnershipClaim,
       where: o.location_id == ^location_id, select: o.user_id)
       true -> {:error, %{id: ["something unexpected happened"]}}
