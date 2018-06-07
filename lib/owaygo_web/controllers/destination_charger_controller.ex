7f84e15d99b47bd443bd05ce649f1bc9fa5e827b
nkaffine <nicholas.kaffine@gmail.com>
2018-06-07 14:32:51 -0400

    updating destination charger controller to fix encoding error

--- lib/owaygo_web/controllers/destination_charger_controller.ex
+++ lib/owaygo_web/controllers/destination_charger_controller.ex
@@ -15,13 +15,19 @@ defmodule OwaygoWeb.DestinationChargerController do
   end
 
   defp render_destination_charger(conn, destination_charger) do
+    address = %{street: destination_charger.location.address.street,
+    city: destination_charger.location.address.city,
+    state: destination_charger.location.address.state,
+    zip: destination_charger.location.address.zip,
+    country: destination_charger.location.address.country}
     {:ok, body} = %{id: destination_charger.location_id,
     name: destination_charger.location.name, lat: destination_charger.location.lat,
     lng: destination_charger.location.lng, tesla_id: destination_charger.tesla_id,
     discoverer_id: destination_charger.location.discoverer_id,
     claimer_id: destination_charger.location.claimer_id,
+    discovery_date: destination_charger.location.discovery_date,
     type: destination_charger.location.type,
-    address: destination_charger.location.address} |> Poison.encode
+    address: address} |> Poison.encode
     conn |> resp(201, body)
   end
 end
