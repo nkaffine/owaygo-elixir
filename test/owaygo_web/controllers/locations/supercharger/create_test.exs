aa62a8e59e55216bd3c7da5cb5962e99d873c253
nkaffine <nicholas.kaffine@gmail.com>
2018-06-12 08:15:36 -0400

    adding optional address to outer layer api for creating super charger tests

--- test/owaygo_web/controllers/locations/supercharger/create_test.exs
+++ test/owaygo_web/controllers/locations/supercharger/create_test.exs
@@ -15,6 +15,12 @@ defmodule OwaygoWeb.Location.Supercharger.CreateTest do
   @status "OPEN"
   @open_date "2014-03-20"
 
+  @street "50 forsyth st."
+  @city "Boston"
+  @state "MA"
+  @zip "02115"
+  @country "United States"
+
   defp create_user() do
     create = %{username: @username, fname: @fname, lname: @lname, email: @email}
     conn = build_conn() |> post("/api/v1/user", create)
@@ -39,7 +45,8 @@ defmodule OwaygoWeb.Location.Supercharger.CreateTest do
     create_type("supercharger")
     create = %{name: @name, lat: @lat, lng: @lng, stalls: @stalls,
     sc_info_id: @sc_info_id, status: @status, open_date: @open_date,
-    discoverer_id: user_id}
+    discoverer_id: user_id, street: @street, city: @city, state: @state,
+    zip: @zip, country: @country}
     conn = build_conn() |> post("/api/v1/location/supercharger", create)
     body = conn |> response(201) |> Poison.decode!
     assert body["id"] |> is_integer
@@ -55,6 +62,11 @@ defmodule OwaygoWeb.Location.Supercharger.CreateTest do
     assert body["discovery_date"] == Date.utc_today |> to_string
     assert body["claimer_id"] == nil
     assert body["type"] == "supercharger"
+    assert body["street"] == @street
+    assert body["city"] == @city
+    assert body["state"] == @state
+    assert body["zip"] == @zip
+    assert body["country"] == @country
   end
 
   test "throws error when given invalid paramters" do
