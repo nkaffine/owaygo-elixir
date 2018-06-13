e70c3737a46b579a6393b5e80f562105b406d6ca
nkaffine <nicholas.kaffine@gmail.com>
2018-06-13 11:06:00 -0400

    implementing controller for creating menu category

---
+++ lib/owaygo_web/controllers/menu_category_controller.ex
@@ -0,0 +1,19 @@
+defmodule OwaygoWeb.MenuCategoryController do
+  use OwaygoWeb, :controller
+
+  alias Owaygo.Location.Restaurant.Menu.Category.Create
+  alias OwaygoWeb.Errors
+
+  def create(conn, params) do
+    attrs = %{name: params["name"], user_id: params["user_id"]}
+    case Create.call(%{params: attrs}) do
+      {:ok, category} -> render_category(conn, category)
+      {:error, changeset} -> Errors.render_error(conn, changeset)
+    end
+  end
+
+  defp render_category(conn, category) do
+    {:ok, body} = %{id: category.id, user_id: category.user_id} |> Poison.encode
+    conn |> resp(201, body)
+  end
+end
