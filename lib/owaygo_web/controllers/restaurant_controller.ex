5504dfa3ecb15cd91725e8df3635d2fb2970b65c
nkaffine <nicholas.kaffine@gmail.com>
2018-06-13 10:07:48 -0400

    updating controller to output correct attributes:

--- lib/owaygo_web/controllers/restaurant_controller.ex
+++ lib/owaygo_web/controllers/restaurant_controller.ex
@@ -7,14 +7,15 @@ defmodule OwaygoWeb.RestuarantController do
     attrs = %{name: params["name"], lat: params["lat"], lng: params["lng"],
     facebook: params["facebook"], twitter: params["twitter"],
     instagram: params["instagram"], website: params["website"],
-    phone_number: params["phone_number"], email: params["email"]}
+    phone_number: params["phone_number"], email: params["email"],
+    discoverer_id: params["discoverer_id"]}
     attrs = if(params["street"] != nil || params["city"] != nil
     || params["state"] != nil || params["zip"] || params["country"]) do
       attrs |> Map.put(:street, params["street"])
       |> Map.put(:city, params["city"])
       |> Map.put(:state, params["state"])
       |> Map.put(:zip, params["zip"])
-      |> Map.put(:countyr, params["country"])
+      |> Map.put(:country, params["country"])
     else
       attrs
     end
@@ -24,22 +25,25 @@ defmodule OwaygoWeb.RestuarantController do
     end
   end
 
-  def render_restaurant(conn, restuarant) do
-    address = if(restuarant.location.address |> Ecto.assoc_loaded?) do
-      %{street: restuarant.location.address.street,
-      city: restuarant.location.address.street,
-      state: restuarant.location.address.street,
-      zip: restuarant.location.address.zip,
-      country: restuarant.location.address.country}
+  def render_restaurant(conn, restaurant) do
+    address = if(restaurant.location.address |> Ecto.assoc_loaded?) do
+      %{street: restaurant.location.address.street,
+      city: restaurant.location.address.city,
+      state: restaurant.location.address.state,
+      zip: restaurant.location.address.zip,
+      country: restaurant.location.address.country}
     else
       nil
     end
-    {:ok, body} = %{name: restuarant.location.name,
-    lat: restuarant.location.lat, lng: restuarant.location.lng,
-    facebook: restuarant.facebook, twitter: restuarant.twitter,
-    instagram: restuarant.instagram, website: restuarant.website,
-    phone_number: restuarant.phone_number, email: restuarant.email,
-    address: address} |> Poison.encode
+    {:ok, body} = %{id: restaurant.location.id,
+    name: restaurant.location.name,
+    lat: restaurant.location.lat, lng: restaurant.location.lng,
+    facebook: restaurant.facebook, twitter: restaurant.twitter,
+    instagram: restaurant.instagram, website: restaurant.website,
+    phone_number: restaurant.phone_number, email: restaurant.email,
+    address: address, discoverer_id: restaurant.location.discoverer_id,
+    discovery_date: restaurant.location.discovery_date,
+    claimer: restaurant.location.claimer_id, type: restaurant.location.type} |> Poison.encode
     conn |> resp(201, body)
   end
 end
