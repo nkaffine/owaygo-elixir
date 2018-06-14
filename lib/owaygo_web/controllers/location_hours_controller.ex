2bc8995b082c4545eab2a6df8718c50b4f178a17
nkaffine <nicholas.kaffine@gmail.com>
2018-06-14 08:28:20 -0400

    fixing error where params were passed without being converted to a map

--- lib/owaygo_web/controllers/location_hours_controller.ex
+++ lib/owaygo_web/controllers/location_hours_controller.ex
@@ -4,8 +4,9 @@ defmodule OwaygoWeb.LocationHoursContoller do
   alias Owaygo.Location.Hours.Create
 
   def create(conn, params) do
-    attrs = %{day: params["day"], hour: params["hour"], opening: params["opening"]}
-    case Create.call(%{params: params}) do
+    attrs = %{day: params["day"], hour: params["hour"], opening: params["opening"],
+    location_id: params["location_id"]}
+    case Create.call(%{params: attrs}) do
       {:ok, hour} -> render_hour(conn, hour)
       {:error, changeset} -> Errors.render_error(conn, changeset)
     end
@@ -14,7 +15,7 @@ defmodule OwaygoWeb.LocationHoursContoller do
   defp render_hour(conn, hour) do
     {:ok, body} = %{id: hour.id, location_id: hour.location_id,
     day: hour.day, hour: hour.hour, opening: hour.opening} |> Poison.encode
-    conn |> resp(201, body) 
+    conn |> resp(201, body)
   end
 
 end
