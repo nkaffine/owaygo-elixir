d16788e021da6fba8ff25b0908d9d20d3945ad7d
nkaffine <nicholas.kaffine@gmail.com>
2018-06-18 11:43:54 -0400

    adding location tag table to database

---
+++ priv/repo/migrations/20180618153000_add_location_tag_table.exs
@@ -0,0 +1,18 @@
+defmodule Owaygo.Repo.Migrations.AddLocationTagTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:location_tag, primary_key: false) do
+      add :average_rating, :float
+      add :location_id, references(:location)
+      add :tag_id, references(:tag)
+      timestamps
+    end
+
+    create unique_index(:location_tag, [:location_id, :tag_id], primary_key: true, name: :location_tag_pair)
+  end
+
+  def down do
+    drop table(:location_tag)
+  end
+end
