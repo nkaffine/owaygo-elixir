bc391b28467d51540f491fa0c77392e6d748335c
nkaffine <nicholas.kaffine@gmail.com>
2018-06-25 13:33:26 -0400

    changing outer layer api test for creating food_item_tags to use food_item_id instead of location_id

--- test/owaygo_web/controllers/tag/food_item/create_test.exs
+++ test/owaygo_web/controllers/tag/food_item/create_test.exs
@@ -11,19 +11,20 @@ defmodule OwaygoWeb.Tag.FoodItem.CreateTest do
     tag = map.tag
     assert {:ok, _email_verification} = Support.verify_email(user)
     assert {:ok, location} = Support.create_location_with_user(user)
-    %{tag: tag, user: user, location: location}
+    assert {:ok, food_item} = Support.create_food_item_with_user_and_location(user, location)
+    %{tag: tag, user: user, location: location, food_item: food_item}
   end
 
   defp create() do
     map = setup()
-    %{location_id: map.location.id, tag_id: map.tag.id}
+    %{food_item_id: map.food_item.id, tag_id: map.tag.id}
   end
 
   test "given valid parameters return a valid response" do
     create = create()
     conn = build_conn() |> post("/api/v1/tag/food-item", create)
     body = conn |> response(201) |> Poison.decode!
-    assert body["location_id"] == create.location_id
+    assert body["food_item_id"] == create.food_item_id
     assert body["tag_id"] == create.tag_id
     assert body["inserted_at"] |> Support.ecto_datetime_to_date_string == Support.today
     assert body["updated_at"] |> Support.ecto_datetime_to_date_string == Support.today
@@ -36,18 +37,18 @@ defmodule OwaygoWeb.Tag.FoodItem.CreateTest do
     _body = conn |> response(201)
     conn = build_conn() |> post("/api/v1/tag/food-item", create)
     body = conn |> response(400) |> Poison.decode!
-    assert body["location"] == nil
+    assert body["food_item_id"] == nil
     assert body["tag_id"] == nil
     assert body["inserted_at"] == nil
     assert body["updated_at"] == nil
-    assert body["food_item_tag"] == ["has already been taken"]
+    assert body["food_item_tag"] == ["already exists"]
   end
 
   test "throw an error when given invalid parameters" do
     create = create() |> Map.delete(:tag_id)
     conn = build_conn() |> post("/api/v1/tag/food-item", create)
     body = conn |> response(400) |> Poison.decode!
-    assert body["location_id"] == nil
+    assert body["food_item_id"] == nil
     assert body["tag_id"] == ["can't be blank"]
     assert body["inserted_at"] == nil
     assert body["updated_at"] == nil
