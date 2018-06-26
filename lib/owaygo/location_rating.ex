f16044fb441d7edb85b8cb86722e43463f9e5d1a
nkaffine <nicholas.kaffine@gmail.com>
2018-06-26 14:50:11 -0400

    adding schema for location ratings

---
+++ lib/owaygo/location_rating.ex
@@ -0,0 +1,11 @@
+defmodule Owaygo.LocationRating do
+  use Ecto.Schema
+
+  schema "location_rating" do
+    field :rating, :float
+    belongs_to :user, Owaygo.User, foreign_key: :user_id, primary_key: true
+    belongs_to :tag, Owaygo.Tag, foreign_key: :tag_id, primary_key: true
+    belongs_to :location, Owaygo.Location, foreign_key: :location_id, primary_key: true
+    timestamps
+  end
+end
