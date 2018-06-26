d7ed12eaf48d7a80416f8e4639c2cabdddd1e14f
nkaffine <nicholas.kaffine@gmail.com>
2018-06-26 12:14:12 -0400

    partial test implementation for inner layer of api for creating location ratings

--- test/contexts/rating/location/create_test.exs
+++ test/contexts/rating/location/create_test.exs
@@ -3,6 +3,7 @@ defmodule Owaygo.Rating.Location.CreateTest do
 
   alias Owaygo.Support
   alias Owaygo.Rating.Location.Create
+  alias Owaygo.User
 
   defp create() do
     {:ok, %{user: user, location: location, tag: tag, location_tag: location_tag}}
@@ -13,7 +14,7 @@ defmodule Owaygo.Rating.Location.CreateTest do
   defp create_without_verification() do
     {:ok, %{user: user, location: location, tag: tag, location_tag: location_tag}}
     = Support.create_location_tag()
-    {:ok, user2} = Support.create_user("411rockstar@gmail.com", "kaffine.n")
+    {:ok, user2} = Support.create_user("kaffine.n", "411rockstar@gmail.com")
     %{location_id: location.id, user_id: user2.id, tag_id: tag.id, rating: 4}
   end
 
@@ -34,44 +35,101 @@ defmodule Owaygo.Rating.Location.CreateTest do
     assert error == errors_on(changeset)
   end
 
+  test "gives valid response with valid parameters" do
+    check_success(create())
+  end
+
   describe "missing parameters" do
-    test "reject when missing location_id"
+    test "reject when missing location_id" do
+      check_error(create() |> Map.delete(:location_id), %{location_id: ["can't be blank"]})
+    end
 
-    test "reject when missing user_id"
+    test "reject when missing user_id" do
+      check_error(create() |> Map.delete(:user_id), %{user_id: ["can't be blank"]})
+    end
 
-    test "reject when missing tag_id"
+    test "reject when missing tag_id" do
+      check_error(create() |> Map.delete(:tag_id), %{tag_id: ["can't be blank"]})
+    end
 
-    test "reject when missing rating"
+    test "reject when missing rating" do
+      check_error(create() |> Map.delete(:rating), %{rating: ["can't be blank"]})
+    end
   end
 
   describe "validity of user_id" do
-    test  "reject when user does not exist"
+    test  "reject when user does not exist" do
+      create = create()
+      check_error(create |> Map.put(:user_id, create.user_id + 1),
+      %{user_id: ["does not exist"]})
+    end
 
-    test "reject when user_id is not an int"
+    test "reject when user_id is not an int" do
+      check_error(create() |> Map.put(:user_id, "jasfgk"),
+      %{user_id: ["is invalid"]})
+    end
 
-    test "reject when user_id is negative"
+    test "reject when user_id is negative" do
+      check_error(create() |> Map.put(:user_id, -124),
+      %{user_id: ["does not exist"]})
+    end
 
-    test "accept when user has not verified their email"
+    test "accept when user has not verified their email" do
+      check_error(create_without_verification(), %{user_id: ["has not verified their email"]})
+    end
 
-    test "accept when user has already rated this location tag"
+    test "accept when user has already rated this location tag" do
+      create = create()
+      rating = check_success(create)
+      updated_at = rating.updated_at
+      rating = check_success(create)
+      refute updated_at == rating.updated_at
+    end
   end
 
   describe "validity of location_id" do
-    test "reject when location_id does not exist"
+    test "reject when location_id does not exist" do
+      create = create()
+      check_error(create |> Map.put(:location_id, create.location_id + 1),
+      %{location_id: ["does not exist"]})
+    end
 
-    test "reject when location_id is not an int"
+    test "reject when location_id is not an int" do
+      check_error(create() |> Map.put(:location_id, "jasfj"),
+      %{location_id: ["is invalid"]})
+    end
 
-    test "reject when location_id is negative"
+    test "reject when location_id is negative" do
+      check_error(create() |> Map.put(:location_id, -1249),
+      %{lcoation_id: ["does not exist"]})
+    end
   end
 
   describe "validity of tag_id" do
-    test "reject when tag_id does not exist"
+    test "reject when tag_id does not exist" do
+      create = create()
+      check_error(create |> Map.put(:tag_id, create.tag_id + 1),
+      %{tag_id: ["does not exist"]})
+    end
 
-    test "reject when tag_id is not an int"
+    test "reject when tag_id is not an int" do
+      check_error(create() |> Map.put(:tag_id, "jafkas"),
+      %{tag_id: ["is invalid"]})
+    end
 
-    test "reject when tag_id is negative"
+    test "reject when tag_id is negative" do
+      check_error(create() |> Map.put(:tag_id, -124),
+      %{tag_id: ["does not exist"]})
+    end
 
-    test "reject when there is no location tag with the given tag_id"
+    test "reject when there is no location tag with the given tag_id tag" do
+      create = create()
+      assert {:ok, tag} = Support.create_tag_with_user(%{%User{} | id: create.user_id}, "some tag")
+      check_error(create |> Map.put(:tag_id, tag.id),
+      %{location_tag: ["does not exist"]})
+    end
+
+    test "reject when there is no location tag with the given tag_id location"
   end
 
   describe "validity of rating" do
