93ae68d13f6c6e767b9f4e0d08b6987a12982111
nkaffine <nicholas.kaffine@gmail.com>
2018-06-26 15:41:26 -0400

    fixing rating to be int instead of float

---
+++ priv/repo/migrations/20180626185118_adding_location_rating_table.exs
@@ -0,0 +1,18 @@
+defmodule Owaygo.Repo.Migrations.AddingLocationRatingTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:location_rating) do
+      add :rating, :integer
+      add :user_id, references(:user)
+      add :location_id, references(:location)
+      add :tag_id, references(:tag)
+      timestamps()
+    end
+    create unique_index(:location_rating, [:user_id, :location_id, :tag_id], primary_key: true)
+  end
+
+  def down do
+    drop table(:location_rating)
+  end
+end
